84dc4424ffb919ac432e601e843df35d0d490fc0
prakul19 <prakulagarwal5@gmail.com>
2025-02-27 12:27:03 +0530

    [Prakul Agarwal] stats: Program for calculating simple interest

---
+++ CalculateSimpleInterest.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class CalculateSimpleInterest {
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.println("Enter principal : ");
+int principal = input.nextInt();
+System.out.println("Enter rate : ");
+int rate = input.nextInt();
+System.out.println("Enter time : ");
+int time = input.nextInt();
+double simpleInterest = (principal * rate * time) / 100;
+System.out.println("the simple interest is " + simpleInterest);
+}
+}
