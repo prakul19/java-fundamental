aa4265fdf55dbc11d6c1024def9f9026217cfcef
prakul19 <prakulagarwal5@gmail.com>
2025-02-27 13:28:57 +0530

    [Prakul Agarwal] stats: program to convert kilometers to miles

---
+++ ConvertKilometersToMiles.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public class ConvertKilometersToMiles {
+public static void main(String[] args){
+Scanner input = new Scanner(System.in);
+System.out.println("Enter distance in kilometers: ");
+double distanceInKilometers = input.nextDouble();
+double distanceInMiles = distanceInKilometers * 0.621371;
+System.out.println("The distance in miles is " + distanceInMiles);
+}
+}
